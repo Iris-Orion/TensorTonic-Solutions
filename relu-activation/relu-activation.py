80ff34aa11d25480233f3b28700a6537c7aa2456
Iris-Orion <56402651+Iris-Orion@users.noreply.github.com>
2026-09-24 15:16:30 +0800

    Solve relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def relu(x) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    # Write code here
+    x_arr = np.asarray(x, dtype=float)
+    return np.where(x_arr>0, x_arr, 0.0)
